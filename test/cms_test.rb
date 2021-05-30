a8e8fd3e9d3dc09a06dad9238f812c4209d79a3e
Dean Wan <deanwan14@gmail.com>
2021-05-30 10:00:40 +1000

    Update: Add tests for markdown

--- test/cms_test.rb
+++ test/cms_test.rb
@@ -16,7 +16,7 @@ class AppTest < Minitest::Test
 
     assert_equal(200, last_response.status)
     assert_equal("text/html;charset=utf-8", last_response["Content-Type"])
-    assert_includes(last_response.body, "about.txt")
+    assert_includes(last_response.body, "about.md")
     assert_includes(last_response.body, "changes.txt")
     assert_includes(last_response.body, "history.txt")
   end
@@ -27,5 +27,32 @@ class AppTest < Minitest::Test
     assert_equal(200, last_response.status)
     assert_equal("text/plain", last_response["Content-Type"])
     assert_includes(last_response.body, "Ruby 0.95 released")
+  end
+
+  def test_invalid_file
+    def generate_code(number)
+      charset = ('a'..'z').to_a
+      Array.new(number) { charset.sample }.join
+    end
+    random_file = "#{generate_code(10)}.txt"
+
+    get "/#{random_file}"
+
+    assert_equal(302, last_response.status)
+    # Request the page that the user was redirected to
+    get last_response["Location"]
+    assert_equal(200, last_response.status)
+    assert_includes(last_response.body, "#{random_file} does not exist")
+
+    get "/" # Reload the page
+    refute_includes(last_response.body, "#{random_file} does not exist")
+  end
+
+  def test_markdown
+    get "/about.md"
+
+    assert_equal(200, last_response.status)
+    assert_equal("text/html;charset=utf-8", last_response["Content-Type"])
+    assert_includes(last_response.body, "<h1>Ruby is...</h1>")
   end
 end
